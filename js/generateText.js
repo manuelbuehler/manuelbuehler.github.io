692b8d8261c327e88d201d45f208a0694394e917
manuelbuehler <manuel.buehler@edu.gbssg.ch>
2023-06-08 22:09:26 +0200

    New Layout on Impressum

--- js/generateText.js
+++ js/generateText.js
@@ -1,7 +1,8 @@
 const chatOutput = document.getElementsByClassName('chat-output')[0];
 
-// Function takes a custom message as input and displays it in the chat container.
+// Die Funktion erhält eine Nachricht und gibt diese gestafflet aus.
 function typeMessage(customMessage) {
+  var punctuationMarks = ['!', '.'];
   var words = customMessage.split(' ');
 
   var i = 0;
@@ -16,15 +17,39 @@ function typeMessage(customMessage) {
       chatOutput.appendChild(wordElement);
       chatOutput.scrollTop = chatOutput.scrollHeight;
 
+      /* Nach jedem Punkt / Ausrufezeichen / ... wird ein Umschlag eingefügt. */
+      var hasPunctuationMark = punctuationMarks.some(mark => currentWord.includes(mark));
+      if (hasPunctuationMark) {
+        chatOutput.innerHTML += '<br>';
+      }
+
       i++;
-    } else {
-      clearInterval(interval);
-      chatOutput.innerHTML += '<br>'; // Umbruch nach jeder Nachricht
     }
-  }, 300);
+  }, 250);
 }
 
-// This function creates a delay in milliseconds before resolving.
+/* Diese Funktion gibt HTML-Links korrekt aus*/
+function typeMessageWithHTMLLinks(customMessage) {
+  var linkContainer = document.createElement('span');
+  linkContainer.innerHTML = customMessage;
+
+  var links = linkContainer.getElementsByTagName('a');
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    link.className = 'code-font-link';
+
+    var spanElement = document.createElement('span');
+    spanElement.appendChild(link);
+
+    chatOutput.appendChild(spanElement);
+    chatOutput.scrollTop = chatOutput.scrollHeight;
+  }
+}
+  
+
+
+
+// Diese Funktion fügt eine Verzögerung ein.
 function wait(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
